dc46e756740c306bb8a0b66c9921239e7f87657f
nmk10000 <58908219+nmk10000@users.noreply.github.com>
2021-06-21 23:19:11 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -22,7 +22,7 @@ async def start(client, message):
                     ],
                     [
                         InlineKeyboardButton(
-                            "⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/TroJanzHEX")
+                            "⭕️ JOIN OUR GROUP ⭕️", url="https://t.me/allmoviesavailble")
                     ]
                 ]
             ),
@@ -45,7 +45,7 @@ async def help(client, message):
                     ],
                     [
                         InlineKeyboardButton(
-                            "⭕️ SUPPORT ⭕️", url="https://t.me/TroJanzSupport")
+                            "⭕️ SUPPORT ⭕️", url="t.me/kevincris0")
                     ]
                 ]
             ),
@@ -68,7 +68,7 @@ async def about(client, message):
                     ],
                     [
                         InlineKeyboardButton(
-                            "SOURCE CODE", url="https://github.com/TroJanzHEX/Auto-Filter-Bot-V2")
+                            "SOURCE CODE", url="t.me/kevincris0")
                     ]
                 ]
             ),
